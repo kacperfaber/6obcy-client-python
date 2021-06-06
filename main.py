72db897bff6601c949891e2aa99f9533042f5bd9
unknown <kacperf1234@gmail.com>
2021-06-06 16:22:59 +0200

    Initial commit.
    Features:
    *) send text
    *) Connect with stranger
    *) Ping
    
    Issues:
    *) There's something that looks bad. When client is typing, in the same
    time that stranger is typing (styp event). These two texts (input and
    'Stranger's writing...' are layed down.

---
+++ main.py
@@ -0,0 +1,95 @@
+import websocket
+import threading
+import time
+import sys
+import os
+import datetime
+
+clientlib = __import__("obcy-client")
+
+class CaptchaVerificationRequest(clientlib.CommandListener):
+    def command_received(self, command):
+        print("Captcha Verification Request!")
+
+
+class MessageReceiver(clientlib.CommandListener):
+    def command_received(self, command):
+        if command.ev_name == "rmsg":
+            message_console_log(command.ev_data["msg"], "STR")
+
+class TypeStateReceiver(clientlib.CommandListener):
+    def command_received(self, command):
+        if command.ev_name == "styp":
+            if command.ev_data:
+                print("Stranger's writing...", end='\r')
+            else:
+                print('', end='\r')
+                sys.stdout.flush()
+
+
+class DisconnectReceiver(clientlib.CommandListener):
+    def command_received(self, command):
+        if command.ev_name == "sdis":
+            global channel
+            channel = None
+            print("Disconnected.\n\n")
+
+
+class ConnectedEventReceiver(clientlib.CommandListener):
+    def command_received(self, command):
+        if command.ev_name == "talk_s":
+            print("Connected.\n\n")
+
+
+sock = websocket.create_connection("wss://server.6obcy.pl:7004/6eio/?EIO=3&transport=websocket")
+socket_client = clientlib.SocketClient(sock)
+socket_client.register_listener("rmsg", MessageReceiver())
+socket_client.register_listener("styp", TypeStateReceiver())
+socket_client.register_listener("sdis", DisconnectReceiver())
+socket_client.register_listener("talk_s", ConnectedEventReceiver())
+socket_client.register_listener("caprecvsas", CaptchaVerificationRequest())
+socket_client.transaction.run_receiver()
+
+client = clientlib.ObcyClient(socket_client)
+channel = None
+
+def message_console_log(message_text, sender):
+     msg = message_text
+     if len(msg) < 23:
+       msg = msg + " " * (23 - len(msg))
+       current_time = datetime.datetime.now().strftime("%H:%M:%S")
+       print(f'{current_time} {sender}: {msg}')
+       sys.stdout.flush() 
+
+def setup_console():
+    os.system("color")
+
+def read_input_loop():
+    while True:
+        x = input()        
+        global channel
+        if x == "/connect":
+            channel = client.find_stranger()
+            continue
+        if channel is not None:
+            if x == "/disc":
+                channel.disconnect()
+                continue
+            channel.send_text_message(x)
+            print ('\033[1A' + '\033[K', end='', flush=True)
+            message_console_log(x, "YOU")
+
+
+def ping_loop():
+    while True:
+        time.sleep(10)
+        client.socket_client.ping()
+
+
+setup_console()
+
+input_thread = threading.Thread(target=read_input_loop)
+input_thread.start()
+
+ping_thread = threading.Thread(target=ping_loop)
+ping_thread.start()
---
+++ obcy-client.py
@@ -0,0 +1,198 @@
+import json
+import threading
+import time
+
+
+class Command:
+    def __init__(self, ev_name, ev_data, opcode):
+        self.ev_name = ev_name
+        self.ev_data = ev_data
+        self.opcode = opcode
+
+
+class CommandListener(object):
+    def command_received(self, command: Command):
+        pass
+
+
+class AddedCommandListener(object):
+    def __init__(self, ev_name, command_listener):
+        self.ev_name = ev_name
+        self.command_listener = command_listener
+
+class IdOffset():
+    def __init__(self, id, offset):
+        self.id = id
+        self.offset = offset
+
+class Transaction(object):
+    def __init__(self, socket_client):
+        self.socket_client = socket_client
+        self.commands = []
+        self.id_objects = []
+        pass
+
+    def run_receiver(self):
+        self.receiver_thread = threading.Thread(target=self.receiver_thread)
+        self.receiver_thread.start()
+
+    def receiver_thread(self):
+        while True:
+           r = self.socket_client.socket.recv()
+           if r == "3" or r.startswith("0"):
+               continue
+           cmd = self.socket_client.parse_command_string(r)
+           self.commands.append(cmd)
+           log = open("log.txt", "a+")
+           log.write(cmd.ev_name + "\n")
+           log.close()
+           matched_listeners = self.socket_client.get_registered_listeners_by_name(cmd.ev_name)
+           for matched_listener in matched_listeners:
+               matched_listener.command_received(cmd)
+
+    def read_command(self, offset):
+        try:
+            return self.commands[offset]
+        except: 
+            return None
+    
+    def get_offset_for_id(self, id):
+        for obj in self.id_objects:
+            if obj.id == id:
+                return obj.offset
+
+    def is_id_exists(self, id):
+        for obj in self.id_objects:
+            if obj.id == id:
+                return True
+        return False
+
+    def make_id(self, id, offset=0):
+        self.id_objects.append(IdOffset(id, offset))
+
+    def increase_offset_for_id(self, id):
+        for obj in self.id_objects:
+            if obj.id == id:
+                obj.offset = obj.offset + 1
+
+
+    def read_command_for_id(self, id):
+        if self.is_id_exists(id) == False:
+            self.make_id(id)
+        try:
+            cmd = self.read_command(self.get_offset_for_id(id))
+            self.increase_offset_for_id(id)
+            return cmd
+        except:
+            time.sleep(1)
+            cmd = self.read_command(self.get_offset_for_id(id))
+            self.increase_offset_for_id(id)
+            return  cmd
+
+class SocketClient:
+    def __init__(self, ws_client):
+        self.transaction = Transaction(self)
+        self.socket = ws_client
+        self.ceid = 1
+        self.listeners = []
+
+    def wait_for_message(self, ev_name):
+        offset = len(self.transaction.commands)
+        while True:
+            cmd = self.transaction.read_command(offset)
+            if cmd == None:
+                time.sleep(1)
+                continue
+            offset = offset + 1
+            if cmd.ev_name != None:
+                if cmd.ev_name == ev_name:
+                    return cmd
+
+    def get_registered_listeners_by_name(self, ev_name):
+        ret = []
+        for listener in self.listeners:
+            if listener.ev_name == ev_name:
+                ret.append(listener.command_listener)
+        return ret
+
+    def read_data_loop(self):
+        pass
+
+    def ping(self):
+        self.socket.send('2')
+
+    def cmd(self, ev_name, ev_data, with_ceid=False):
+        d = dict(ev_name=ev_name, ev_data=ev_data)
+        if with_ceid:
+            d['ceid'] = self.ceid
+        self.socket.send('4' + json.dumps(d))
+        pass
+
+    def parse_command_string(self, command_string):
+        if command_string.startswith("0"):
+            return None
+        if command_string.startswith("4"):
+            d = json.loads(command_string[1:])
+            return Command(d["ev_name"], d["ev_data"], 4)
+        if command_string == "3":
+            return None
+        return None
+
+    def get_cmd(self, message):
+        if isinstance(message, Command):
+            return message
+        return Command(message["ev_name"], message["ev_data"], 0)
+        
+
+    def register_listener(self, ev_name, command_listener: CommandListener):
+        self.listeners.append(AddedCommandListener(ev_name, command_listener))
+
+
+class ChannelClient:
+    def __init__(self, socket_client, ckey, cid):
+        self.socket_client = socket_client
+        self.ckey = ckey
+        self.cid = cid
+        self.idn = 0
+        self.disconnected = False
+
+    def increment_idn(self):
+        self.idn = self.idn + 1
+
+    def send_text_message(self, text):
+        self.socket_client.cmd("_pmsg", dict(ckey=self.ckey, idn=self.idn, msg=text), with_ceid=True)
+        self.increment_idn()
+        pass
+
+    def read_text_message(self):
+        cmd = self.socket_client.get_cmd(self.socket_client.wait_for_message("rmsg"))
+        return cmd.ev_data["msg"]
+
+    def disconnect(self):
+        self.socket_client.cmd("_distalk", dict(ckey=self.ckey), with_ceid=True)
+        self.disconnected = True
+
+
+class ObcyClient:
+    def __init__(self, socket_client):
+        self.socket_client = socket_client
+
+    def ping(self):
+        self.socket_client.send("2")
+
+    def ping_loop(self, offset):
+        while True:
+            time.sleep(offset)
+            self.ping()
+
+    def run_ping_thread(self, offset=15):
+        ping_thread = threading.Thread(target=self.ping_loop, args=[offset])
+        ping_thread.start()
+
+    def find_stranger(self) -> ChannelClient:
+        self.socket_client.cmd("_sas",
+                               dict(channel="main", myself=dict(sex=0, loc=16), preferences=dict(sex=0, loc=16)),
+                               with_ceid=False)
+        talk_s_message = self.socket_client.wait_for_message("talk_s")
+        talk_command = self.socket_client.get_cmd(talk_s_message)
+        return ChannelClient(self.socket_client, talk_command.ev_data['ckey'], talk_command.ev_data["cid"])
